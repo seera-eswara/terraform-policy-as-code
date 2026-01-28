0253428179ee64cc3fe8a043e1efb9da0e03d857
Eswar <eswar@company.com>
2026-01-28 08:19:52 -0500

    Update outputs to use standardized platform naming

--- modules/landing-zone-policies/outputs.tf
+++ modules/landing-zone-policies/outputs.tf
@@ -1,18 +1,18 @@
 output "policy_definition_ids" {
   description = "IDs of created policy definitions"
   value = {
-    cloudinfra_allowed_vm_skus    = azurerm_policy_definition.allowed_vm_skus_cloudinfra.id
-    cloudinfra_naming_convention  = azurerm_policy_definition.naming_convention_cloudinfra.id
+    platform_allowed_vm_skus    = azurerm_policy_definition.allowed_vm_skus_platform.id
+    platform_naming_convention  = azurerm_policy_definition.naming_convention_platform.id
     lz_allowed_vm_skus            = azurerm_policy_definition.allowed_vm_skus_lz.id
     lz_naming_convention          = azurerm_policy_definition.naming_convention_lz.id
   }
 }
 
-output "cloudinfra_policy_assignments" {
-  description = "IDs of policy assignments for cloudinfra MG"
+output "platform_policy_assignments" {
+  description = "IDs of policy assignments for platform MG"
   value = {
-    allowed_vm_skus = var.enable_cloudinfra_policies ? azurerm_management_group_policy_assignment.cloudinfra_allowed_skus[0].id : null
-    naming          = var.enable_cloudinfra_policies ? azurerm_management_group_policy_assignment.cloudinfra_naming[0].id : null
+    allowed_vm_skus = var.enable_platform_policies ? azurerm_management_group_policy_assignment.platform_allowed_skus[0].id : null
+    naming          = var.enable_platform_policies ? azurerm_management_group_policy_assignment.platform_naming[0].id : null
   }
 }
 
